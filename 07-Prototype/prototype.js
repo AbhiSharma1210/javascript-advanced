6effe802e5fe431b07d884d62714d687ce376433
Abhinav Sharma <abhi5237@gmail.com>
2024-10-06 01:27:55 +0530

    inheritance

--- 07-Prototype/prototype.js
+++ 07-Prototype/prototype.js
@@ -18,6 +18,28 @@ Object.prototype.myName = function () {
 randomObject.myName();
 bikes.myName();
 
+// Inheritance
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
 // modern syntax
 // 
 Object.setPrototypeOf(TeachingSupport, Teacher)
